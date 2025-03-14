8c1384f777ceb52287d236f1a2280c17ae919c57
Tiana-K08 <tetkononenko2020@gmail.com>
2025-03-14 18:36:01 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,45 @@
+# Exercise 1:
+my_string = 'Tetiana Kononenko'
+my_number = 100
+my_list = ['one', 'two', 'three', 'four']
+my_bullean = True
+print(my_string, my_number, my_list, my_bullean)
+
+# Exercise 2:
+first_letters = my_string[0:3]
+print(first_letters)
+print(my_string[0:3])
+
+# Exercise 3:
+first_element = my_string[0]
+print(first_element)
+print(my_string[0])
+
+# Exercise 4:
+new_number = my_number + 10
+print(new_number)
+print(my_number + 10)
+
+# Exercise 5:
+last_element = my_list[-1]
+print(last_element)
+print(my_list[-1])
+
+# Exercise 6:
+names = 'harry,alex,susie,jared,gail,conner'
+new_list_names = names.split(',')
+print(new_list_names)
+print(names.split(','))
+
+# Exercise 7:
+first_word = my_string[0:7].upper()
+print(first_word)
+new_string = first_word + my_string[7:]
+print(new_string)
+
+# Exercise 8:
+my_string_with_number = f'My favorite number is {my_number}'
+print(my_string_with_number)
+
+# Exercise 9:
+print('hello world')
